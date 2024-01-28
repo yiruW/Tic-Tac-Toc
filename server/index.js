c688f57ebbdc102ff0fd08a25321c33390057525
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 12:18:54 -0500

    [build] Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const express = require('express')
+const connectDB = require('./config/db');
 const port = process.env.PORT || 8000
+
+connectDB()
 
 const app = express()
 
