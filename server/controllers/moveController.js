24c0a5af74aafa58a2b2c947ae0d1ee2790d1789
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 13:54:15 -0500

    [backend] Add move validation to move controller

--- server/controllers/moveController.js
+++ server/controllers/moveController.js
@@ -1,20 +1,20 @@
 const asyncHandler = require('express-async-handler')
 const { ObjectId } = require('mongodb');
+const Game = require('../models/gameModel')
 const Move = require('../models/moveModel')
-const { createLog } = require('./logsController')
+const { createLog } = require('./logsController');
+const { findGameById } = require('./gameController');
 
-
-const createMove = async (newMove) => {
-    const move = Move.create(newMove);
-    return move;
-}
-
+/*
+    Method: POST /api/move
+    return: 
+*/
 const handleMove = asyncHandler( async (req, res) => {
     const {gameId, player, location} = req.body;
     const log = { gameId: new ObjectId(gameId), message: '', isEventValid: true };
 
     try {
-        if (isValid) {
+        if (isValid(gameId, player, location)) {
             const newMove = { gameId: new ObjectId(gameId), player: player, isEventValid: true };
             await createMove(newMove);
             if (isWin) {
@@ -22,6 +22,7 @@ const handleMove = asyncHandler( async (req, res) => {
             } else if (isDraw) {
                 message = `Game Ends in a Draw`;
             } else {
+                // update Game Player
                 message = `Player ${player} made a valid move`;
             }
             createLog({...log, message: message});
@@ -37,8 +38,50 @@ const handleMove = asyncHandler( async (req, res) => {
     }
 })
 
-const isValid = () => {
-    return true
+// Insert Move Record to DB
+const createMove = async (newMove) => {
+    const move = Move.create(newMove);
+    return move;
+}
+
+// Fetch All the Moves under Game #
+const getMovesByGameId = async (gameId) => {
+    try {
+        return await Move.find({ gameId });
+    } catch (error) {
+        console.error('Error fetching moves: ', error);
+        throw error;
+    }
+}
+
+const isCurrentPlayerTurn = async (gameId, player) => {
+    try {
+        const currentGame = await findGameById(gameId);
+        if (currentGame) {
+            return currentGame.currentPlayer === player;
+        }
+        return false;
+    } catch (error) {
+        console.error('Error checking player valid: ', error);
+        throw error;
+    }
+}
+
+const isMoveLocationValid = async (gameId, location) => {
+    try {
+        const moves = await getMovesByGameId(new ObjectId(gameId));
+        return !moves.some(move => move.location === location);
+    } catch (error) {
+        console.error('Error checking move location:', error);
+        throw error;
+    }
+};
+
+// Check Move Valid - Player & Location
+const isValid = async (gameId, player, location) => {
+    const isPlayerValid = await isCurrentPlayerTurn(gameId, player);
+    const isLocationValid = await isMoveLocationValid(gameId, location);
+    return isPlayerValid && isLocationValid
 }
 
 const isWin = () => {
