4c8080de6dada9dbb888d873217934a4671e61e9
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 15:36:06 -0500

    [fontend] add handleCellClick method, add snackbar notification

--- client/app/components/GameBoard.tsx
+++ client/app/components/GameBoard.tsx
@@ -3,11 +3,15 @@ import { Button, Container, Grid } from '@mui/material';
 import React, { useState } from 'react';
 import GameCell from './GameCell';
 import axios from 'axios';
+import SnakeBar from './SnakeBar';
   
 export default function GameBoard({}: {}){
     const [gameId, setGameId] = useState('')
     const [board, setBoard] = useState<string[][]>([['', '', ''], ['', '', ''], ['', '', '']]);
     const [currentPlayer, setCurrentPlayer] = useState<string>('X');
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarType, setSnakebarType] = useState<'success' | 'error'>('success');
+    const [snakebarMessage, setsnakebarMessage] = useState('');
 
     const restartGame = async () => {
         try {
@@ -20,10 +24,44 @@ export default function GameBoard({}: {}){
             console.error('Error restarting game:', error);
         }
     }
-    const handleCellClick = async (row: number, col: number) => {}
+
+    const showSnackbar = (type: 'success' | 'error', message: string) => {
+        setSnakebarType(type);
+        setsnakebarMessage(message);
+        setSnackbarOpen(true);
+    };
+
+    const handleCellClick = async (row: number, col: number) => {
+        axios.post('http://localhost:8000/api/move', {
+            gameId: gameId,
+            player: currentPlayer,
+            location: row * 3 + col
+        }).then((response) => {
+            // handle valid move
+            const newBoard = board.map((r, ri) => 
+                r.map((c, ci) => (ri === row && ci === col ? currentPlayer : c))
+            );
+            setBoard(newBoard);
+            setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+            showSnackbar( 'success', response.data.msg)
+        }).catch((error) => {
+            if (error.response!.status === 400) {
+                // handle invalid move
+                showSnackbar( 'error', error.response?.data?.message)
+            } else {
+                console.error('Error making move:', error)
+            }
+        })
+    }
 
     return (
         <Container sx={{ mt: '80px' }}>
+            <SnakeBar
+                open={snackbarOpen}
+                handleClose={() => setSnackbarOpen(false)}
+                message={snakebarMessage}
+                type={snackbarType}
+            />
             <Grid container direction="row" justifyContent="center"  alignItems="center">
                 {board.map((row, rowIndex) => (
                     <Grid container item key={rowIndex} justifyContent="center">
