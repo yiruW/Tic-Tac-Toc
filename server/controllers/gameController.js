d4e605d217bfc233b99707b1c5edbce93972d4dc
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 13:52:00 -0500

    Revert "[backend] Add move validation"
    This reverts commit 7028bb5196c87fe028d2a742ac846bf94f669eee.

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require('express-async-handler')
 const Game = require('../models/gameModel')
-const { ObjectId } = require('mongodb');
 
 /*
     Method: POST /api/game
@@ -13,7 +12,7 @@ const startGame = asyncHandler( async (req, res) => {
 
 const findGameById = async(gameId) => {
     try{
-        const game = await Game.findById(new ObjectId(gameId)).lean()
+        const game = await Game.findById(new ObjectId(gameId))
         return game
     } catch(error) {
         throw error
