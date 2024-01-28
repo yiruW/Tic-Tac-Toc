1b2662b0c2b15fbcef57e8734614f00a94f1fc29
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 15:14:03 -0500

    [backend] Add cors

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const port = process.env.PORT || 8000
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/game', require('./routes/gameRoutes'))
-app.use('/api/logs', require('./routes/logsRoutes'))
-app.use('/api/move', require('./routes/moveRoutes'))
+app.use('/api/game', require('./routes/gameRoutes'));
+app.use('/api/logs', require('./routes/logsRoutes'));
+app.use('/api/move', require('./routes/moveRoutes'));
 
 app.listen(port, () => console.log(`sever run on port ${port}`))
