162fb58254628ad6a127499ec892862034dc7295
Rachael <rachaelwu.ca@gmail.com>
2024-01-28 13:18:08 -0500

    [backend][refactor] refactor createLog to internal method

--- server/controllers/logsController.js
+++ server/controllers/logsController.js
@@ -11,20 +11,10 @@ const getLogs = asyncHandler( async (req, res) => {
     res.status(200).json(logs)
 })
 
-/*
-    Method: POST /api/logs
-    Return: Log[]
-*/
-const createLog = asyncHandler( async (req, res) => {
-    const {gameId, message, isEventValid} = req.body
-    console.log(gameId, message, isEventValid)
+const createLog = asyncHandler( async (newLog) => {
     try {
-        const log = await Log.create({
-            gameId: new ObjectId(gameId), 
-            message: message,
-            isEventValid: isEventValid
-        })
-        res.status(200).json(log)
+        const log = await Log.create(newLog)
+        return log
     } catch (error) {
         throw error
     }
